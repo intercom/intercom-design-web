1a17e81f754b8a6ea8917892db51f1e4f95e30ed
“tanya-ermlva” <hi@ermolaeva.co>
2025-06-06 13:21:31 +0100

    content added

--- data/articles.js
+++ data/articles.js
@@ -44,19 +44,20 @@ export const articlesData = {
         "title": "TEXTS",
         "articles": [
             {
-                "title": "Intercom on Product: How we became an AI-first company",
-                "author": "Des Traynor",
-                "url": "https://www.intercom.com/blog/intercom-on-product-how-we-became-an-ai-first-company/"
+                "title": "A new age of UX: Evolving your design approach for AI products",
+                "author": "Molly Mahar",
+                "url": "https://www.intercom.com/blog/design-ux-machine-learning-ai/"
             },
             {
                 "title": "Intercom on Product: DeepSeek, agents, and the future of AI with Benedict Evans",
                 "author": "Liam Geraghty",
                 "url": "https://www.intercom.com/blog/videos/intercom-on-product-benedict-evans-ai/"
             },
+            
             {
-                "title": "A new age of UX: Evolving your design approach for AI products",
-                "author": "Molly Mahar",
-                "url": "https://www.intercom.com/blog/design-ux-machine-learning-ai/"
+                "title": "Intercom on Product: How we became an AI-first company",
+                "author": "Des Traynor",
+                "url": "https://www.intercom.com/blog/intercom-on-product-how-we-became-an-ai-first-company/"
             },
             
             
@@ -66,10 +67,27 @@ export const articlesData = {
         "title": "TALKS & EVENTS",
         "articles": [
             {
-                "title": "Why Old UI Designs Won't Work with AI",
-                "author": "Emma Wilson",
+                "title": "Why Old UI Designs Won’t Work with AI | Off Script",
+                "author": "Emmet Connolly",
                 "url": "https://www.youtube.com/watch?v=I8hMPj3AD34"
-            }
+            },
+            {
+                "title": "How to design standout products in an AI world",
+                "author": "Molly Mahar",
+                "url": "https://www.youtube.com/watch?v=rc1WGS8QNeI"
+            },
+            {
+                "title": "Working at Intercom with Tatiana Sivo | Product Designer",
+                "author": "Tatiana Sivo",
+                "url": "https://www.youtube.com/watch?v=L4qvhTuoO4A&list=PLlCIldMZCaFq2FOT-aQC8H7YL4xHSaQbB&index=8"
+            },
+            {
+                "title": "Working at Intercom with Murtaza Abidi | Senior Product Designer",
+                "author": "Murtaza Abidi",
+                "url": "https://www.youtube.com/watch?v=3rdWaH-M8f8&list=PLlCIldMZCaFq2FOT-aQC8H7YL4xHSaQbB&index=9"
+            },
+            
+            
         ]
     }
 }; 
